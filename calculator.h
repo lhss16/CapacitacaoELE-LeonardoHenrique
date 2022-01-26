585b258df9df2a7eda1f9e162ef966213db9eb91
lhss16 <lhss16@hotmail.com>
2022-01-25 23:20:18 -0300

    fixing types of use

--- calculator.h
+++ calculator.h
@@ -4,9 +4,6 @@
 //necessario para utilizar o objeto da classe string que contem o valor passado pelo usuario
 #include <string>
 
-//input eh um tipo generico para que a Calculator funcione com qualquer numero de entrada
-template <typename input>
-
 //classe que contem as operacoes e metodos necessarios para a operacao da Calculator
 class Calculator
 {
@@ -19,25 +16,34 @@ class Calculator
         Calculator(char operation, std::string numberA, std::string numberB);
         
         //metodo publico para calculo de uma operacao passada de um operando
-        input calc(char operation, input numberA);
+        int calc(char operation, int numberA);
+        double calc(char operation, double numberA);
         //metodo publico para calculo de uma operacao passada de dois operandos
-        input calc(char operation, input numberA, input numberB);
+        int calc(char operation, int numberA, int numberB);
+        double calc(char operation, double numberA, double numberB);
 
     private:
         //metodo privado que verifica se a entrada eh int (retorna true) ou double (retorna false)
-        bool verifyInputIsInt(std::string numberA);
+        bool verifyintIsInt(std::string numberA);
         
         //metodos privados com as operacoes de dois operandos
-        input add (input numberA, input numberB);       //x+y
-        input sub (input numberA, input numberB);       //x-y
-        input multiply (input numberA, input numberB);  //x*y
-        input divide (input numberA, input numberB);    //x/y
-        input mod(input numberA, input numberB);        //x%y
-        input exp(input numberA, input numberB);        //x^y
+        int add (int numberA, int numberB);       //x+y
+        int sub (int numberA, int numberB);       //x-y
+        int multiply (int numberA, int numberB);  //x*y
+        int divide (int numberA, int numberB);    //x/y
+        int mod(int numberA, int numberB);        //x%y
+        int exp(int numberA, int numberB);        //x^y
 
+        double add (double numberA, double numberB);       //x+y
+        double sub (double numberA, double numberB);       //x-y
+        double multiply (double numberA, double numberB);  //x*y
+        double divide (double numberA, double numberB);    //x/y
+        double exp(double numberA, double numberB);        //x^y
         //metodos privados com as operacoes de um operando
-        input root(input numberA);       //x^(1/2)
-        input fatorial(input numberA);                  //x*(x-1)*(x-2)....*1
-        input fibonacci(input numberA);                 //x(n-1) + x(n-2)
+        unsigned int root(unsigned int numberA);       //x^(1/2)
+        double root(double numberA);       //x^(1/2)
+
+        unsigned int fatorial(unsigned int numberA);                  //x*(x-1)*(x-2)....*1
+        unsigned int fibonacci(unsigned int numberA);                 //x(n-1) + x(n-2)
 };
 #endif
